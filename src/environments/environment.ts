ec8e572839fde88dfeee9b2c9e180386c72711fb
Marcelo <marcelo.tadeu@castgroup.com.br>
2020-01-31 13:26:16 -0300

    ajusta .ico

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  apiBaseUrl: 'http://localhost:8080',
+  apiBaseUrl: 'https://sisms-backend.herokuapp.com/',
   production: false
 };
 /*
