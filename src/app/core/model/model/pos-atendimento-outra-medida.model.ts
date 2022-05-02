2bb7c4adc4160dfaa251de13b06e3ada94ced672
Marcelo <marcelo.souza.81@hotmail.com>
2022-05-02 20:49:38 -0300

    implementa funcionalidade de visualizar o ultimo atendimento

--- src/app/core/model/model/pos-atendimento-outra-medida.model.ts
+++ src/app/core/model/model/pos-atendimento-outra-medida.model.ts
@@ -4,6 +4,7 @@ export class PosAtendimentoOutraMedida {
     constructor(
         public id?: number,
         public posAtendimentoId?: number,
+        public outraMedidaDescricao?: string,
         public outraMedidaId?: number,
         public valor?: number
     ) { }
